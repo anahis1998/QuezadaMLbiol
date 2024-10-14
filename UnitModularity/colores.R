1eec948a683fe1aeeaf63c45f7707084481e75f6
anahis1998 <asquezada04@gmail.com>
2024-10-13 22:30:04 -0500

    Adding my colors function

---
+++ UnitModularity/colores.R
@@ -0,0 +1,17 @@
+#function to select a palette of colors 
+#The target here is define a function with two options of colors to use in a plot
+#This way, I can decide if I want the plot with color blind friendly colors or not
+
+
+colores <- function(type = "blind_friendly", n = 50) {
+  
+  if (type == "blind_fr") {
+    colors <- viridis::viridis(n)
+  } else if (type == "non_blind_fr") {
+    colors <- rainbow(n)
+  } else {
+    stop("error")
+  }
+  
+  return(colors)
+}
