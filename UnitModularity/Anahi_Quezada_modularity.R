19d99d57ae1445a3f5c2fee7aca8c7161b7a0a94
anahis1998 <asquezada04@gmail.com>
2024-10-13 21:59:48 -0500

    defining the modl. examples

--- UnitModularity/Anahi_Quezada_modularity.R
+++ UnitModularity/Anahi_Quezada_modularity.R
@@ -66,11 +66,12 @@ slopes_2 <- full_join(slopes_p, slopes_t, by= c("state", "name",
                                                 "lon", "lat"))
 hist(slopes_2$slope_p_1)
 hist(slopes_2$slope_t_1)
-#Map using the maps package
+#To do the map I used ggplot2
+grad <- na.omit(slopes_2)
 map_1 <- map_data("state")
 ggplot() +
-  geom_polygon(data = map_1, aes(x = long, y = lat, group = group), fill = "lightgrey", color = "white") +
-  geom_point(data = slopes_2, aes(x = lon, y = lat, color = slopes_2$slope_p_1), size = 2) +
+  geom_polygon(data = map_1, aes(x = long, y = lat, group = group), fill = "lightblue", color = "white") +
+  geom_point(data = slopes_2, aes(x = lon, y = lat, color = grad$slope_p_1), size = 2) +
   scale_color_gradient(low = "yellow", high = "blue", na.value = "grey") +  
   labs(title = "Map of Precipitation Slopes", color = "Slope (Precip)") +
   theme_minimal() +
@@ -83,11 +84,4 @@ ggplot() +
   theme_minimal() +
   coord_fixed(1.3)
 
-map(database = slopes_2$slope_p_1, regions = slopes_2, exact = FALSE, boundary = TRUE,
-    interior = TRUE, parameters = NULL, orientation = NULL,
-    fill = FALSE, col = 1, plot = TRUE, add = FALSE, namesonly = FALSE,
-    xlim = NULL, ylim = NULL, wrap = FALSE, resolution = if (plot) 1 else 0,
-    type = "l", bg = par("bg"), mar = c(4.1, 4.1, par("mar")[3], 0.1),
-    myborder = 0.01, namefield="name", lforce="n")
-map('usa', region= slopes_2$slope_p_1)
-map('counties')
+
