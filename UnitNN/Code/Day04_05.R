4cf62358448dcd75b2b0d093ca94a47e120dd520
anahis1998 <asquezada04@gmail.com>
2024-11-24 22:10:59 -0600

    creating my day 4 to 5 script

---
+++ UnitNN/Code/Day04_05.R
@@ -0,0 +1,6 @@
+#Day 4 and day 5
+
+#In this code, I'm going to include more arguments to improve the development 
+#of my previous code. Since, I'm working with images, I can play more with 
+#arguments such as rotation, brightness, zoom, color, size, and resize. 
+
